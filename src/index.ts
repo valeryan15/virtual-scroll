ad5598cba71afdd9e57c71c3ac631b04f8c815a7
valeryan15 <valeryan15@gmail.com>
2026-02-01 18:50:42 +0700

    feat: add shared public types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export {};
+export type {
+  AxisConfig,
+  GridOverscan,
+  GridRange,
+  Overscan1D,
+  Overscan2D,
+  Range1D,
+  Range2D,
+  ScrollAlign,
+  ScrollBehavior,
+  ScrollPosition,
+  ScrollToIndexOptions,
+} from './shared/types';
