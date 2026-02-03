d0e0f42fc0911f4ca0a15ae9603ce937598d2396
valeryan15 <valeryan15@gmail.com>
2026-02-03 11:50:46 +0700

    chore(test): migrate jest to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -32,50 +32,12 @@ export default defineConfig({
     projects: [
       {
         extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, '.storybook'),
-          }),
-        ],
         test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: 'chromium',
-              },
-            ],
-          },
-          setupFiles: ['.storybook/vitest.setup.ts'],
-        },
-      },
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, '.storybook'),
-          }),
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: 'chromium',
-              },
-            ],
-          },
-          setupFiles: ['.storybook/vitest.setup.ts'],
+          name: 'unit',
+          environment: 'jsdom',
+          setupFiles: ['vitest.setup.ts'],
+          include: ['src/**/*.spec.{ts,tsx}'],
+          globals: true,
         },
       },
       {
